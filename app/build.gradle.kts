527c0e605af2c8d9574c65a18b9128fb972c8ed9
Alejandro Fort <alejandrofortt@gmail.com>
2024-08-01 21:08:52 -0500

    chore: add new dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.serialization)
+    alias(libs.plugins.safeargs.kotlin)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+
+    kotlin("kapt")
 }
 
 android {
@@ -45,4 +51,45 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Architecture Components
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+
+    // Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Coil
+    implementation(libs.coil)
+
+    // Retrofit
+    implementation(platform(libs.retrofit2.bom))
+    implementation(libs.retrofit2.core)
+    implementation(libs.retrofit2.converter.kotlinSerialization)
+
+    // OkHttp
+    implementation(platform(libs.okhttp3.bom))
+    implementation(libs.okhttp3.okhttp)
+    implementation(libs.okhttp3.loggingInterceptor)
+
+    // Kotlin Serialization
+    implementation(libs.kotlinx.serialization.json)
+
+    implementation(libs.androidx.core.splashscreen)
+
+    // Timber
+    implementation(libs.timber)
 }
